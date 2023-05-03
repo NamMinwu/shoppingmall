8238c6b9dfeb6fd43bb422d473aa9a0b6ecb9673
NamMinwu <minwu06255@gmail.com>
2023-05-03 17:34:34 +0900

    CHORE : later fix it

--- src/routes/api/orders/server.ts
+++ src/routes/api/orders/server.ts
@@ -6,7 +6,7 @@ export const POST = (async (event) => {
 	const data = await event.request.json();
 	const prismaClient = new PrismaClient();
 	const todo = await prismaClient.cart.create({
-		data: { title: data.title, content: data.content }
+		data: { title: data. , content: data. }
 	});
 	return json(todo);
 }) satisfies RequestHandler;
