2cd9ad9a73da6c2ccee69bb9b91c142b6ed64457
NamMinwu <minwu06255@gmail.com>
2023-05-04 12:41:38 +0900

    FIX : change cart array type to product model

--- src/lib/stores/store.ts
+++ src/lib/stores/store.ts
@@ -1,12 +1,15 @@
-import { writable } from 'svelte/store';
-export type TItem = {
-	name: string;
-	price: number;
-};
-export const products = writable<TItem[]>([
-	{ name: '사과', price: 1000 },
-	{ name: '바나나', price: 2000 },
-	{ name: '딸기', price: 3000 }
-]);
+// import { writable } from 'svelte/store';
+// export type TItem = {
+// 	name: string;
+// 	price: number;
+// };
+// export const products = writable<TItem[]>([
+// 	{ name: '사과', price: 1000 },
+// 	{ name: '바나나', price: 2000 },
+// 	{ name: '딸기', price: 3000 }
+// ]);
 
-export const cart = writable<TItem[]>([]);
+import { writable } from 'svelte/store';
+import type { Product } from '@prisma/client';
+
+export const cart = writable<Product[]>([]);
