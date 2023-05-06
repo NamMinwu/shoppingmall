fe432076d2ed05daee93f5ea3e0dc6073f6c52a0
NamMinwu <minwu06255@gmail.com>
2023-05-06 11:02:43 +0900

    FEAT : woke css make accordion and make components

--- src/routes/mypage/+page.svelte
+++ src/routes/mypage/+page.svelte
@@ -1,28 +1,81 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
+	import Accordion from './Accordion.svelte';
 
 	export let data: PageServerData;
 </script>
 
-<h1>my page</h1>
-<span>{data.user.id}</span>
-<span>{data.user.email}</span>
-<span>{data.user.name}</span>
-<a href="logout">logout</a>
-{#if data.orders !== undefined}
-	<div class="order-list">
-		<h3>Order-List</h3>
-		<ul>
-			{#each data.orders as order}
-				<li>
-					<span>{order.updatedAt}</span>
-				</li>
-				{#each order.products as product}
-					<li>
-						<span>{product.name}</span>
-					</li>
-				{/each}
-			{/each}
-		</ul>
+<div class="mypageContainer">
+	<div class="userInfo">
+		<h1>my page</h1>
+		<span>{data.user.email}</span>
+		<span>{data.user.name}</span>
+		<a href="logout">logout</a>
 	</div>
-{/if}
+	{#if data.orders !== undefined}
+		<div class="order-list">
+			<h3>Order-List</h3>
+			<div>
+				{#each data.orders as order}
+					<Accordion title={order.updatedAt.toString()}>
+						{#each order.products as product}
+							<div class="product">
+								<span>{product.name}</span>
+							</div>
+						{/each}
+					</Accordion>
+				{/each}
+			</div>
+		</div>
+	{/if}
+</div>
+
+<style>
+	.mypageContainer {
+		margin-top: -70px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		height: 100vh;
+	}
+	.userInfo {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.userInfo > span {
+		color: #231f1e;
+		font-family: Inter, sans-serif;
+		font-weight: 500;
+		margin-bottom: 10px;
+		font-size: 18px;
+	}
+	.userInfo > a {
+		color: #231f1e;
+		font-family: Inter, sans-serif;
+		font-size: 18px;
+		font-weight: 500;
+		text-decoration: none;
+	}
+	.userInfo > a:hover {
+		color: rgb(0, 61, 41);
+		font-family: Inter, sans-serif;
+		font-weight: 500;
+		font-size: 18px;
+		text-decoration: underline;
+	}
+	.order-list {
+		display: flex;
+		flex-direction: column;
+	}
+	.product {
+		padding: 10px;
+		color: #231f1e;
+		font-family: Inter, sans-serif;
+		font-size: 18px;
+		font-weight: 500;
+	}
+</style>
---
+++ src/routes/mypage/Accordion.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import type { Order, Product } from '@prisma/client';
+	export let title: string;
+	let isOpen: boolean = false;
+	function toggleList() {
+		isOpen = !isOpen;
+	}
+</script>
+
+<div class="toggleButton">
+	<button on:click={toggleList}>{title}</button>
+</div>
+<div class="list">
+	{#if isOpen}
+		<slot />
+	{/if}
+</div>
+
+<style>
+	.toggleButton > button {
+		padding: 10px;
+		font-family: Inter, sans-serif;
+		color: #333;
+		margin-top: 5px;
+		margin-left: 5px;
+		background-color: white;
+		font-size: 14px;
+		font-weight: 550;
+		border-style: solid;
+		border-width: 1px;
+		border-radius: 20px;
+		border-color: #333;
+	}
+	.toggleButton > button:hover {
+		padding: 10px;
+		font-family: Inter, sans-serif;
+		color: white;
+		margin-top: 5px;
+		margin-left: 5px;
+		background-color: rgb(0, 61, 41);
+		font-size: 14px;
+		font-weight: 550;
+		border-style: solid;
+		border-width: 1px;
+		border-radius: 20px;
+		border-color: rgb(0, 61, 41);
+		cursor: pointer;
+		transition: all 300ms ease;
+	}
+	.list {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
